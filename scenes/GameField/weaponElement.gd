149880d60dba84d391b98b9c898dfc8db85a9667
Артур Михалев <ultraartgtx@gmail.com>
2019-04-27 15:07:53 +0700

    ui weapone
    ui weapone

---
+++ scenes/GameField/weaponElement.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+#warning-ignore:unused_class_variable
+export var x = 0 
+
+func _on_Area2D_input_event():
+	print("11")
+	 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
