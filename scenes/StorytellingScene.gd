7dd29990dd1143d0eb33ef966ef1e207e1a7f573
Артур Михалев <ultraartgtx@gmail.com>
2019-04-28 17:04:58 +0700

    add storyTile
    add storyTile

--- scenes/StorytellingScene.gd
+++ scenes/StorytellingScene.gd
@@ -5,10 +5,37 @@ extends Node2D
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
+#func o
+var currentSpriteid = 1 
+
+func setVisibleStory():
+		if(currentSpriteid==1):
+			currentSpriteid=2
+			$story1.visible=false
+			$story2.visible=true
+			$story3.visible=false
+		else: 
+			currentSpriteid=3
+			$story1.visible=false
+			$story2.visible=false
+			$story3.visible=true
+#   if(currentSpriteid==3):
+##	загрузить сцену
+	
+
 func _ready():
 	pass 
 	# Replace with function body.
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		setVisibleStory()
+		
+var tmp = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	tmp+=delta
+	if(tmp>=5.0):
+		setVisibleStory()
+		tmp=0.0
+	
